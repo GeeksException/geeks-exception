9b79d5037f02a685c9d77f5f9687186c852addfc
gengstah <gerardpdelasarmas@gmail.com>
2014-02-18 20:29:54 +0800

    Design layout for What we do page and mobile layout

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	//$("html,body").stop().animate();
-	
-	//$(".competence").addClass("started");
 	$(".section-title").addClass("started");
-	//$("header.fade").fadeIn("slow");
-	//$("section#comment").slideDown("slow");
+	$(".competence").addClass("started");
 });
