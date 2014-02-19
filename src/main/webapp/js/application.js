a986f17366b6a997539dc7a8ae9be25569e38800
Gerard Delas Armas <gerardpdelasarmas@gmail.com>
2014-02-19 12:26:26 +0800

    Update application.js

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -1,5 +1,12 @@
+(function(a){a.expr[":"].onScreen=function(b){var c=a(window),d=c.scrollTop(),e=c.height(),f=d+e,g=a(b),h=g.offset().top,i=g.height(),j=h+i;return h>=d&&h<f||j>d&&j<=f||i>e&&h<=d&&j>=f}})(jQuery);
+
 $(document).ready(function(){
-	$(".section-title").addClass("started");
-	$(".competence").addClass("started");
-	$(".about").addClass("started");
+	a();
+	$(window).scroll(function(){ a(); });
 });
+
+function a() {
+	$(".section-title").filter(":onScreen").addClass("started");
+	$(".competence").filter(":onScreen").addClass("started");
+	$(".about").filter(":onScreen").addClass("started");
+}
